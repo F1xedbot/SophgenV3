e7dc76ad35e5b992ae6cf6657010ca1686aa4da2
F1xedbot <therealfixedbot@gmail.com>
2025-10-11 14:37:15 +0700

    feat: add llm models enum

--- src/utils/enum.py
+++ src/utils/enum.py
@@ -3,3 +3,9 @@ from enum import StrEnum
 class LLMProvider(StrEnum):
     GOOGLE = "google"
     OPENAI = "openai"
+
+class LLMModels(StrEnum):
+    GEMINI_2_5_FLASH_LITE = "gemini-2.5-flash-lite"
+    GEMINI_2_5_FLASH = "gemini-2.5-flash"
+    GPT_4_O_MINI = "gpt-4o-mini"
+    GPT_3_5_TURBO = "gpt-3.5-turbo"
