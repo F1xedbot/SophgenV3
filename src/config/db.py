e4afffcc194b6a0b387b8f561d54f704874b90ae
F1xedbot <therealfixedbot@gmail.com>
2025-10-12 00:47:31 +0700

    fix: db config now will resolve to current root folder to ensure consistency

--- src/config/db.py
+++ src/config/db.py
@@ -1,26 +1,35 @@
 from pathlib import Path
 import os
 from helpers.pydantic_to_sql import pydantic_model_to_create_table_sql
-from schema.agents import InjectionSchema
+from schema.agents import InjectionSchema, ValidationSchema
+from utils.enum import AgentTable
 
 class SQLITEConfig:
     """
     SQLite database configuration.
     """
-    PROJECT_ROOT: Path = Path(os.getenv("PROJECT_ROOT", Path.cwd()))
-    DB_PATH: Path = PROJECT_ROOT / os.getenv("SQLITE3_PATH", "data/sophgen.db")
+    PROJECT_ROOT = Path(os.getenv("PROJECT_ROOT", ".")).resolve()
+    DB_PATH = (PROJECT_ROOT / os.getenv("SQLITE3_PATH", "data/sophgen.db")).resolve()
 
     EXTRA_COLUMNS = [("func_name", "TEXT", True)]
 
     INJECTIONS_SQL = pydantic_model_to_create_table_sql(
         InjectionSchema,
-        table_name="injections",
+        table_name=AgentTable.INJECTOR,
         extra_columns=EXTRA_COLUMNS,
         add_id_pk=True,
         add_timestamp=True
     )
 
-    TABLES_SQL = [INJECTIONS_SQL]
+    VALIDATIONS_SQL = pydantic_model_to_create_table_sql(
+        ValidationSchema,
+        table_name=AgentTable.VALIDATOR,
+        extra_columns=EXTRA_COLUMNS,
+        add_id_pk=True,
+        add_timestamp=True
+    )
+
+    TABLES_SQL = [INJECTIONS_SQL, VALIDATIONS_SQL]
 
     @classmethod
     def get_db_path(cls) -> str:
