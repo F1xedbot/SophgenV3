31280257f714965c8491257c22ef431b27c4bc64
F1xedbot <therealfixedbot@gmail.com>
2025-10-11 17:35:17 +0700

    del: remove env.ps1

--- env.ps1
+++
@@ -1,6 +0,0 @@
-# env.ps1
-# Setup environment for SophGenV3
-# Add src folder to PYTHONPATH
-$env:PYTHONPATH = "$PWD\src"
-$env:GRPC_VERBOSITY = "NONE"
-Write-Host "Environment loaded for SophGenV3"
