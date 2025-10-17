88e573cebe5027e88bf3587f0a084fb99e92944b
F1xedbot <therealfixedbot@gmail.com>
2025-10-17 10:55:46 +0700

    feat: add researcher state

--- src/agents/states.py
+++ src/agents/states.py
@@ -1,5 +1,10 @@
-from schema.agents import Context
+from schema.agents import Context, ResearcherSchema
+from typing import Optional
 from langgraph.prebuilt.chat_agent_executor import AgentStatePydantic
 
 class InjectorState(AgentStatePydantic):
     context: Context
+
+class ResearcherState(AgentStatePydantic):
+    cwe_id: str
+    structured_response: Optional[ResearcherSchema] = None
