1375232d3312ad46f2d7ef18880aba921c4c3616
F1xedbot <therealfixedbot@gmail.com>
2025-10-17 17:05:58 +0700

    feat: add decorator for critical tools

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -2,3 +2,8 @@ def exclude_tool(func):
     """Mark a tool method as excluded from get_tools()."""
     func._exclude_tool = True
     return func
+
+def critical_tool(func):
+    """Mark a tool method as critical"""
+    func._critical_tool = True
+    return func
