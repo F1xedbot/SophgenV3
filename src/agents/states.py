e39e70f28d7d07d2685ee055f1dc593b50479101
F1xedbot <therealfixedbot@gmail.com>
2025-10-17 11:16:23 +0700

    chore: remove redundant state keys

--- src/agents/states.py
+++ src/agents/states.py
@@ -1,5 +1,4 @@
-from schema.agents import Context, ResearcherSchema
-from typing import Optional
+from schema.agents import Context
 from langgraph.prebuilt.chat_agent_executor import AgentStatePydantic
 
 class InjectorState(AgentStatePydantic):
@@ -7,4 +6,3 @@ class InjectorState(AgentStatePydantic):
 
 class ResearcherState(AgentStatePydantic):
     cwe_id: str
-    structured_response: Optional[ResearcherSchema] = None
