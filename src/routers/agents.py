61f7bcd361957453a720e52669a71553e4c8634b
F1xedbot <therealfixedbot@gmail.com>
2025-11-07 10:56:19 +0700

    fix: update router

--- src/routers/agents.py
+++ src/routers/agents.py
@@ -7,7 +7,6 @@ from agents.tools import InjectorTools
 from services.local.cache import load_local_cache
 from utils.filter import filter_dict_fields
 from schema.agents import Context
-from agents.states import InjectorState
 from schema.requests import InjectorRequest
 
 router = APIRouter()
@@ -19,19 +18,13 @@ async def run_injector(req: InjectorRequest):
     cwe_fields = ["cwe_id", "cwe_name", "vulnerable_code_patterns", "code_injection_points"]
     cwe_details = filter_dict_fields(req.cwe_ids.split(','), loaded_cwe_cache, cwe_fields)
 
-    injector_tools = InjectorTools()
-    injector_agent = Injector(llm, injector_tools)
-
-    initial_state = InjectorState(
-        messages=[],
-        context=Context(
+    injector_agent = Injector(llm)
+    try:
+        response = await injector_agent.run(context=Context(
             func_code=req.func_code,
             rois=req.rois,
             cwe_details=cwe_details
-        )
-    )
-    try:
-        response = await injector_agent.run(initial_state)
+        ))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
