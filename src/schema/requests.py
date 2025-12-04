8eaa9413cc244b03b2011eaa70f28ba21bad4afc
F1xedbot <therealfixedbot@gmail.com>
2025-12-04 23:57:56 +0700

    feat: update request schema for router

--- src/schema/requests.py
+++ src/schema/requests.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel
 
 class InjectorRequest(BaseModel):
+    func_name: str
     func_code: str
+    lines: str
     rois: str
     cwe_ids: str
+
+class ValidatorRequest(BaseModel):
+    func_name: str
+    func_code: str
+    merged_roi_lines: str
+
+class ResearcherRequest(BaseModel):
+    cwe_id: str
